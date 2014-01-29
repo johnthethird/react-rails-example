d836725e6686cf3b268309cf248716a2f860ff17
John Lynch <john@rigelgroupllc.com>
2014-01-29 09:19:14 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReactRailsExample::Application.config.secret_key_base = '51d28494a0f1f0c475ffeb8141b19e93cc43b34ba3f2d8e849d525d7976e71aaa000b078eca0b5b43b9b4a0d04a30d1d04f9a554e202cec97415f2284c4bd121'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReactRailsExample::Application.config.session_store :cookie_store, key: '_react-rails-example_session'
